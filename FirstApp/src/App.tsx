8f026cec1e2a41e4e0809aaa2626bad64a217768
Arbaj Shaikh <sarbaj637@gmail.com>
2025-12-24 09:30:56 +0530

    learn state management in typeScript

--- FirstApp/src/App.tsx
+++ FirstApp/src/App.tsx
@@ -4,13 +4,7 @@ import MyButton from "./Components/MyButton";
 function App() {
   return (
     <>
-      <MyButton
-        text="Click Me"
-        onClick={() => {
-          alert("Button Clicked!!");
-        }}
-      />
-      <MyButton text="Click " />
+      <MyButton text="Click Me" />
     </>
   );
 }
--- FirstApp/src/Components/MyButton.tsx
+++ FirstApp/src/Components/MyButton.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface MyButton {
   text: String | number | boolean;
   onClick?: () => void;
 }
 
+const [value, setValue] = useState<number>(1);
+
 const MyButton: React.FC<MyButton> = (props) => {
-  const { text, onClick } = props;
-  return <button onClick={onClick}>{text}</button>;
+  const { text } = props;
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={() => setValue((prev) => prev + 1)}>{text}</button>
+    </div>
+  );
 };
 
 export default MyButton;
